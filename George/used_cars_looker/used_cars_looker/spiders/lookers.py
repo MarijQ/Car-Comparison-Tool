508250dca7df19ae6ff0016ce6e90a626950ab6c
GGkakos <139121686+GGkakos@users.noreply.github.com>
2024-11-20 20:51:29 +0000

    updated scraper

--- George/used_cars_looker/used_cars_looker/spiders/lookers.py
+++ George/used_cars_looker/used_cars_looker/spiders/lookers.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy_splash import SplashRequest
+import json
 
 class LookerSpider(scrapy.Spider):
     name = 'lookers'
@@ -48,68 +49,84 @@ class LookerSpider(scrapy.Spider):
     }
 end
     """
-
-    # Start Requests
-    def start_requests(self):
-        
-
-        url = 'https://www.lookers.co.uk/used-car/vauxhall/corsa/1-0-ecoflex-s-3dr/id/669019'
-        yield SplashRequest(
-            url=url,
-            callback=self.parse_car,
-            endpoint="execute",
-            args={'lua_source': self.lua_script},
-            splash_headers={'Content-Type': 'application/json'}
-        )
     
 
-    # Parse the Response
+# Start with the AJAX URL
+    start_urls = [
+        'https://api.lookers.co.uk/vehiclesearch/vehicle/search?Group=d90377b7-3fdf-460c-8090-10cf0207b7b4&Type=Car&condition=Dealer%20New%2C%20Nearly%20New%2CUsed&ignoreFacet=condition&skip=0&sortGroup=d90377b7-3fdf-460c-8090-10cf0207b7b4&sortOrder=Recommended&srsltid=AfmBOopLXJJMuc30je3HW7swmdHe5qYWNhIPeG1gw7RzM26fQZmZYyQE&take=24'
+    ]
+
+    def parse(self, response):
+        # Parse the JSON response
+        data = json.loads(response.text)
+        total_matches = data.get('Matches', 0)
+        offset = data.get('Offset', 0) + len(data.get('Results', []))
+
+        # Generate car-specific URLs and yield requests
+        for car in data.get('Results', []):
+            car_name = car.get('Make').lower().replace(" ", "-")
+            car_model = car.get('Model').lower().replace(" ", "-")
+            car_id = car.get('Id')
+            
+            car_url = f'https://www.lookers.co.uk/used-car/{car_name}/{car_model}/id/{car_id}'
+
+            # Yield a request to scrape the car details
+            yield SplashRequest(
+                url=car_url,
+                method="GET",
+                callback=self.parse_car,
+                endpoint="execute",
+                args={'lua_source': self.lua_script},
+                splash_headers={'Content-Type': 'application/json'},
+                meta={
+                    "id": car.get("Id"),
+                    "price": car.get("Price"),
+                    "make": car.get("Make"),
+                    "model": car.get("Model"),
+                    "mileage": car.get("Odometer"),
+                    "fuel_type": car.get("FuelType"),
+                    "body_style": car.get("BodyStyle"),
+                    "engine_size": car.get("EngineSize"),
+                    "transmission": car.get("Transmission"),
+                    "year": car.get("ModelYear"),
+                    "dealership_name": car.get("DealershipName"),
+                    "registered_date": car.get("RegisteredDate"),
+                }
+            )
+        
+        # Handle pagination
+        if offset < total_matches:
+            next_url = f"https://api.lookers.co.uk/vehiclesearch/vehicle/search?Group=d90377b7-3fdf-460c-8090-10cf0207b7b4&Type=Car&condition=Dealer%20New%2C%20Nearly%20New%2CUsed&ignoreFacet=condition&skip={offset}&sortGroup=d90377b7-3fdf-460c-8090-10cf0207b7b4&sortOrder=Recommended&srsltid=AfmBOopLXJJMuc30je3HW7swmdHe5qYWNhIPeG1gw7RzM26fQZmZYyQE&take=24"
+            yield scrapy.Request(url=next_url, callback=self.parse)
+
     def parse_car(self, response):
-        
-        
-        vehicle_name = response.xpath("//div[contains(@class, 'vehicle-header__variant vehicle-header__desktop-only')]/text()").get().strip()
-        vehicle_stats = response.xpath("//div[contains(@class, 'vehicle-header__stats vehicle-header__desktop-only')]/text()").get().strip()
-    
-        # car's price
-        price = response.xpath("//div[contains(@class, 'vehicle-header__price--full-value')]//span/text()").get().strip()
-        
-        # car's information (miles, type of transimission etc)
+        # Extract car details passed via meta
+        car_meta = response.meta
+
+        # Extract additional car details from the detailed page
         car_info = response.xpath("//div[contains(@class, 'used-specs__data-col')]").css('span.used-specs__vehicle-data::text').getall()
         
-        
-        miles = car_info[0]
-        year = car_info[1]
-        fuel = car_info[2]
-        mpg = car_info[3]
-        engine_size = car_info[4]
-        previous_owners = car_info[5]
-        transmission = car_info[6]
-        n_doors = car_info[7]
-        registration = car_info[8]
+        mpg = car_info[3] 
+        previous_owners = car_info[5] 
+        n_doors = car_info[7] 
         droplet = car_info[9]
-            
-        
-        # car's features list
+
         feature_list = []
         feature_panel = response.css('ul.feature-panel__ul')
         for item in feature_panel:
             items = [text.strip() for text in item.css('li::text').getall()]
             feature_list.extend(items)
-            
 
-        yield {
-            "car_name": vehicle_name,
-            "vehicle_stats": vehicle_stats,
-            "price": price,
-            "miles": miles,
-            "model_year" : year,
-            "fuel": fuel,
+        # Combine meta and scraped data
+        car_data =  {
+            **car_meta,
             "mpg": mpg,
-            "engine_size": engine_size,
             "previous_owners": previous_owners,
-            "transmission": transmission,
-            "n_doors" : n_doors,
-            "registration": registration,
-            "droplet":droplet,
+            "n_doors": n_doors,
+            "droplet": droplet,
             "feature_list": feature_list,
         }
+        
+        car_data.pop('splash', None)
+        
+        yield car_data
